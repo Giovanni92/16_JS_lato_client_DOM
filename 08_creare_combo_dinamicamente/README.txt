5e8fb832cdf5ac4235faafad81318f4f7f1df8c2
Giovanni <desiderio.giovanni92@gmail.com>
2020-10-23 16:36:29 +0200

    CreateElement
    creiamo elementi nel DOM

---
+++ 08_creare_combo_dinamicamente/README.txt
@@ -0,0 +1,5 @@
+Questo esempio doveva dare come output questo:
+
+https://jsbin.com/cohalu/edit?html,css,js,output
+
+sembra che il comando sleect non venga preso
---
+++ 08_creare_combo_dinamicamente/main.js
@@ -0,0 +1,47 @@
+// //per accedere al DOM dobbiamo verificare che il DOM sia pronto
+//  //function è la funzione che verrà chiamara quando il DOM è pronto
+// window.addEventListener('DOMContentLoaded', function(){
+
+//     //vogliamo accedere a dati dalla pagina html
+//     var frm = document.getElementById('dati');
+//     console.log(frm);
+
+//     //creo un array per creare una combo dinamicamente
+//     var colors = ['red', 'green', 'blue'];
+
+//     //select è l'elementpo della combo
+//     var combo = document.createElement('select');
+
+//         while(colors.length){
+//         var colore = colors.pop();
+//             //Option è anche una funzione globale
+//             //il valoree di Option è lo stesso testo che verra usato nella combo
+//             var opt = new Option(colore,colore);
+//             combo.options[combo.options.length]= opt;
+//         }
+
+// frm.appendChild(combo);
+
+
+// });
+
+window.addEventListener('DOMContentLoaded',
+                       function(){
+  
+  
+  var f = document.body;
+console.log(f);
+   colors = ['red','green','blue'];
+ 
+    var combo = document.createElement('select');
+
+  while(colors.length){
+  var colore = colors.pop();
+   var opt = new Option(colore,colore);
+      opt.style.backgroundColor = colore;
+  combo.options[combo.options.length]= opt;
+}
+  f.appendChild(combo);
+  
+});
+
