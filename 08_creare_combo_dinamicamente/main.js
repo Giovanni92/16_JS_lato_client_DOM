2ddcfb1faf0e7deefa31d46dcabb4e3471b43397
Giovanni <desiderio.giovanni92@gmail.com>
2020-10-26 22:06:34 +0100

    Update main.js
    aggiunte mod

--- 08_creare_combo_dinamicamente/main.js
+++ 08_creare_combo_dinamicamente/main.js
@@ -1,6 +1,6 @@
 
-// //per accedere al DOM dobbiamo verificare che il DOM sia pronto
-//  //function è la funzione che verrà chiamara quando il DOM è pronto
+//per accedere al DOM dobbiamo verificare che il DOM sia pronto
+ //function è la funzione che verrà chiamara quando il DOM è pronto
 // window.addEventListener('DOMContentLoaded', function(){
 
 //     //vogliamo accedere a dati dalla pagina html
@@ -26,19 +26,24 @@
 
 // });
 
-
+//per accedere al DOM dobbiamo verificare che il DOM sia pronto
+ //function è la funzione che verrà chiamara quando il DOM è pronto
 window.addEventListener('DOMContentLoaded',
                        function(){
   
-  
-  var f = document.body;
-console.log(f);
-   colors = ['red','green','blue'];
+     //vogliamo accedere a body della pagina html
+    var f = document.body;
+    console.log(f);
+
+    //creo un array per creare una combo dinamicamente
+    colors = ['red','green','blue'];
  
+    //select è l'elementpo della combo
     var combo = document.createElement('select');
 
   while(colors.length){
   var colore = colors.pop();
+  //il valoree di Option è lo stesso testo che verra usato nella combo
    var opt = new Option(colore,colore);
       opt.style.backgroundColor = colore;
   combo.options[combo.options.length]= opt;
